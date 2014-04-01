88672d0092c3fb25b02bdc26726b7b3e463a97ae
diatmpravin <pravinmishra88@gmail.com>
2014-04-01 15:22:23 +0530

    updated Get, Post, Put and Delete Methods

--- restapi.go
+++ restapi.go
@@ -1,24 +1,64 @@
 package restapi
 
 import (
-  "fmt"
-  "log"
-  "net/url"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 )
 
 type Rest struct {
-
+	Header *http.Header
 }
 
 func (r *Rest) Get(r_url string) string {
-  fmt.Println("----In restapi Get----")
-  
-  uri, err := url.Parse(r_url)
-  if err != nil {
-    log.Fatal(err)
-  }
+	fmt.Println("----In restapi Get----")
 
-  fmt.Printf("%#v", uri)
+	uri, err := url.Parse(r_url)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  return "true"
+	fmt.Printf("%#v", uri)
+
+	return "true"
+}
+
+func (r *Rest) Post(r_url string) string {
+	fmt.Println("----In restapi Post----")
+
+	uri, err := url.Parse(r_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v", uri)
+
+	return "true"
+}
+
+func (r *Rest) Put(r_url string) string {
+	fmt.Println("----In restapi Put----")
+
+	uri, err := url.Parse(r_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v", uri)
+
+	return "true"
+}
+
+func (r *Rest) Delete(r_url string) string {
+	fmt.Println("----In restapi Delete----")
+
+	uri, err := url.Parse(r_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%#v", uri)
+
+	return "true"
 }
