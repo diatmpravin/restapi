e7da6b6286113845923367490c5f783cf29de341
diatmpravin <pravinmishra88@gmail.com>
2014-04-01 16:29:46 +0530

    dry code

--- restapi.go
+++ restapi.go
@@ -3,22 +3,27 @@ package restapi
 import (
 	"fmt"
 	"log"
-	"net/http"
+	// "net/http"
 	"net/url"
 )
 
 type Rest struct {
-	Header *http.Header
+	// Header *http.Header
 }
 
-func (r *Rest) Get(r_url string) string {
-	fmt.Println("----In restapi Get----")
-
+func parse_url(r_url string) *url.URL {
 	uri, err := url.Parse(r_url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	return uri
+}
+
+func (r *Rest) Get(r_url string) string {
+	fmt.Println("----In restapi Get----")
+
+	uri := parse_url(r_url)
 	fmt.Printf("%#v", uri)
 
 	return "true"
@@ -27,11 +32,7 @@ func (r *Rest) Get(r_url string) string {
 func (r *Rest) Post(r_url string) string {
 	fmt.Println("----In restapi Post----")
 
-	uri, err := url.Parse(r_url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	uri := parse_url(r_url)
 	fmt.Printf("%#v", uri)
 
 	return "true"
@@ -40,11 +41,7 @@ func (r *Rest) Post(r_url string) string {
 func (r *Rest) Put(r_url string) string {
 	fmt.Println("----In restapi Put----")
 
-	uri, err := url.Parse(r_url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	uri := parse_url(r_url)
 	fmt.Printf("%#v", uri)
 
 	return "true"
@@ -53,11 +50,7 @@ func (r *Rest) Put(r_url string) string {
 func (r *Rest) Delete(r_url string) string {
 	fmt.Println("----In restapi Delete----")
 
-	uri, err := url.Parse(r_url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	uri := parse_url(r_url)
 	fmt.Printf("%#v", uri)
 
 	return "true"
